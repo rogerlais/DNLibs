600ca40d3423c233f160238203e88a58d066d47f
rogerlais <rogerlais@gmail.com>
2017-08-10 20:58:12 -0300

    setup incial via VS casa

--- DNBRRules/Class1.cs
+++ DNBRRules/Class1.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DNBRRules
-{
-    public class Class1
-    {
+﻿namespace DNBRRules {
+    public class Class1 {
     }
 }
+
